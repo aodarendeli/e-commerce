fe2918aa068e2067646ff04e02aec560d4d1f7c8
berkin <berkin-akkaya@hotmail.com>
2023-10-07 23:46:38 +0300

    login page

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import { Spinner } from 'react-bootstrap'
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
 
 function Loader() {
     return (
-        <Spinner animation='border' role='status' style={{ width: '100px', height: '100px', margin: 'auto', display: 'block' }} >
-            <span className='sr-only'>Loading</span>
-        </Spinner>
+        <div style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: 1000,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backdropFilter: 'blur(5px)' // Bu satır arka planı bulanıklaştıracak
+        }}>
+            <Spinner animation='border' role='status' style={{ width: '100px', height: '100px', display: 'block' }}>
+                <span className='sr-only'>Loading</span>
+            </Spinner>
+        </div>
     )
 }
 
-export default Loader
+export default Loader;
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,98 +1,123 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Form, Button, Row, Col, Container } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Message from '../../components/Message/Message'
-import Loader from '../../components/Loader/Loader'
-import { selectUserInfo, selectLoadingState, selectErrorState, LoginUser, checkAdmin, selectAdmin, controlUser } from '../../store/auth'
-import { useLocation, useNavigate } from 'react-router-dom';
-import jwt from 'jwt-decode';
-import './login.scss'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col, Container, Image } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../../components/Message/Message";
+import Loader from "../../components/Loader/Loader";
+
+import {
+  selectUserInfo,
+  selectLoadingState,
+  selectErrorState,
+  LoginUser,
+  checkAdmin,
+  selectAdmin,
+  controlUser,
+} from "../../store/auth";
+import { useLocation, useNavigate } from "react-router-dom";
+import jwt from "jwt-decode";
+import "./login.scss";
 
 function Login() {
-    const [username, seUsername] = useState("")
-    const [password, setPassword] = useState("")
+  const [username, seUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    let location = useLocation();
-    let navigate = useNavigate();
-    const dispatch = useDispatch();
+  let location = useLocation();
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const redirect = location.search ? location.search.split('=')[1] : '/'
-    const userInformation = useSelector(selectUserInfo)
-    const user = userInformation && jwt(userInformation.data)
-    localStorage.setItem("userGuid",user?.Guid)
-    localStorage.setItem("userName",user?.Name)
-    const selectAdmins = useSelector(selectAdmin)
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+  const userInformation = useSelector(selectUserInfo);
+  const user = userInformation && jwt(userInformation.data);
+  localStorage.setItem("userGuid", user?.Guid);
+  localStorage.setItem("userName", user?.Name);
+  const selectAdmins = useSelector(selectAdmin);
 
+  let userVal = selectAdmins?.data?.find((val) => val?.username == user?.Name);
 
-    let userVal = selectAdmins?.data?.find(val => val?.username == user?.Name)
-    
-    useEffect(() => {
-        if (userVal) {
-            dispatch(controlUser(userVal))
-        }
-    }, [userVal, dispatch])
-
-
-    const error = useSelector(selectErrorState)
-    const loading = useSelector(selectLoadingState)
-
-    useEffect(() => {
-        if (userInformation) {
-            navigate(redirect)
-        }
-    }, [navigate, userInformation, redirect])
-
-    const submitHandler = (e) => {
-        e.preventDefault()
-        dispatch(LoginUser({ username, password }))
-        dispatch(checkAdmin())
-        // dispatch(controlUser(selectAdmins))
+  useEffect(() => {
+    if (userVal) {
+      dispatch(controlUser(userVal));
     }
+  }, [userVal, dispatch]);
 
-    // useEffect(() => {
-    //     dispatch(controlUser(userVal))
-    // }, [dispatch])
+  const error = useSelector(selectErrorState);
+  const loading = useSelector(selectLoadingState);
 
-    return (
-        <Container className='sign-container'>
-            <h6 className='mt-2 mb-2'>Sign In</h6>
-            {
-                error && <Message variant='danger'>{error}</Message>
-            }
-            {loading && <Loader />}
-            <Form onSubmit={submitHandler}>
-                <Form.Group controlId='usernmae'>
-                    <Form.Control
-                        type='text'
-                        placeholder='username'
-                        value={username}
-                        onChange={(e) => seUsername(e.target.value)}>
-                    </Form.Control>
+  useEffect(() => {
+    if (userInformation) {
+      navigate(redirect);
+    }
+  }, [navigate, userInformation, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(LoginUser({ username, password }));
+    dispatch(checkAdmin());
+  };
+
+  return (
+    <>
+      {loading && <Loader />}
+      <Container fluid className="full-height p-0">
+        <Row className="full-height">
+          <Col md={9} className="photo-background full-height"></Col>
+          <Col
+            md={3}
+            className="d-flex justify-content-center align-items-center custom-login-box"
+          >
+            <div className="login-content">
+              <h6 className="mt-2 mb-2 text-center">Giriş Yap</h6>
+              {error && <Message variant="danger">{error}</Message>}
+              <Form
+                onSubmit={submitHandler}
+                className="d-flex flex-column align-items-end"
+              >
+                <Form.Group controlId="usernmae">
+                  <Form.Control
+                    className="small-input custom-input"
+                    type="text"
+                    placeholder="Kullanıcı Adı"
+                    value={username}
+                    onChange={(e) => seUsername(e.target.value)}
+                  ></Form.Control>
                 </Form.Group>
-                <Form.Group controlId='password' className='mt-3'>
-                    <Form.Control
-                        type='password'
-                        placeholder='enter password'
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}>
-                    </Form.Control>
+                <Form.Group controlId="password" className="mt-3">
+                  <Form.Control
+                    className="small-input custom-input"
+                    type="password"
+                    placeholder="Şifre"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></Form.Control>
                 </Form.Group>
 
-                <Button className="btn-dark mt-3" type='submit' variant='primary'>
-                    Sign In
+                <Button
+                  className="btn-dark mt-3"
+                  type="submit"
+                  variant="primary"
+                >
+                  Sign In
                 </Button>
-            </Form>
-            <Row className='py-3'>
-                <Col>
-                    New Customer? {' '}
-                    <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-                        Register
-                    </Link>
+              </Form>
+              <Row className="py-3">
+                <Col className="d-flex justify-content-end">
+                  New Customer?{" "}
+                  <Link
+                    to={
+                      redirect ? `/register?redirect=${redirect}` : "/register"
+                    }
+                  >
+                    Register
+                  </Link>
                 </Col>
-            </Row>
-        </Container>
-    )
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
-export default Login
+export default Login;
